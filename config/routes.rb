398db2062d24e73ae56bc3494e795df50b954b2a
jodillion <dillionjoann@gmail.com>
2019-08-20 17:46:11 -0400

    updated route, ran migration

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :breweries, only [:index]
+      resources :breweries, only: [:index]
     end
   end
 end

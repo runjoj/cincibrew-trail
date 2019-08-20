ef9240ecbbc485f6a28d1945bc05312f31847b8c
jodillion <dillionjoann@gmail.com>
2019-08-20 18:07:10 -0400

    removed switch from router

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import { BrowserRouter, Route, Router, Switch, History } from 'react-router-dom'
+import { BrowserRouter, Route, Router } from 'react-router-dom'
 import BreweriesContainer from '../containers/BreweriesContainer'
 
 
 const App = (props) => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/breweries" component={BreweriesContainer} />
-      </Switch>
+      <Route exact path="/breweries" component={BreweriesContainer} />
     </BrowserRouter>
   )
 }

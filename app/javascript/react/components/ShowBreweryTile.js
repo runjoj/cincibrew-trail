ce570a08931d0b199954351f7dc2b4875f48fbec
jodillion <dillionjoann@gmail.com>
2019-08-23 16:45:48 -0400

    wrote ShowBreweryTile

---
+++ app/javascript/react/components/ShowBreweryTile.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ShowBreweryTile = props => {
+
+  return(
+    <div>
+      <img src={props.logo.url} alt="brewery logo" width="200" height="200">
+      <h6>{props.name}</h6>
+      <h6>{props.address} {props.city}, {props.state}</h6>
+      <h6>{props.phone}</h6>
+      <h6>{props.website}</h6>
+    </div>
+  )
+}
+
+export default ShowBreweryTile

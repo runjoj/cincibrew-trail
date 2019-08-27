c10a0d7923e406c8199fab388db757be19d4be28
jodillion <dillionjoann@gmail.com>
2019-08-27 19:00:13 -0400

    add class names and css for index page responsive

--- app/javascript/react/components/BreweryTile.js
+++ app/javascript/react/components/BreweryTile.js
@@ -5,15 +5,17 @@ const BreweryTile = props => {
   let location
 
   return(
-      <div>
-        <Link to={`/breweries/${props.id}`}>
-          <div className="brewery-logo">
-            <img src={props.logo.url} alt="brewery logo" width="200" height="200"/>
-            <div className="brewery-name">
-              <p>{props.name}</p>
+      <div className="brewery-tile col-lg-3">
+        <div className="brewery-tile-contents">
+          <Link to={`/breweries/${props.id}`}>
+            <div className="brewery-logo">
+              <img src={props.logo.url} alt="brewery logo" width="200" height="200"/>
+              <div className="brewery-name">
+                <p>{props.name}</p>
+              </div>
             </div>
-          </div>
-        </Link>
+          </Link>
+        </div>
       </div>
   )
 }
